e158093262d76d2d95d3b707be72ba571c07af95
Alex Archer <alex.archer@gmail.com>
2022-12-15 15:34:31 +0300

    updates

---
+++ tmux/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# install Tmux Plugin Manager (TPM)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+~/.tmux/plugins/tpm/bin/update_plugins all
+
+# Installing plugins
+# Add new plugin to ~/.tmux.conf with set -g @plugin '...'
+# Press prefix + I (capital i, as in Install) to fetch the plugin.
